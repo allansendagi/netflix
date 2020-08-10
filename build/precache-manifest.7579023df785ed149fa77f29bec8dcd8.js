363b7f54f62a476f1d2b0dded9620d1e22bffcdd
allansendagi <sendagiallan87@gmail.com>
2020-08-10 15:32:45 +0400

    adding code

---
+++ build/precache-manifest.7579023df785ed149fa77f29bec8dcd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b61e3ae794cf4e1e4c800f157d3194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "070c0dd36ab4b15e5d35",
+    "url": "/static/css/main.50bddc7e.chunk.css"
+  },
+  {
+    "revision": "0dddcf58b5ab2a8b1351",
+    "url": "/static/js/2.5d212469.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.5d212469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070c0dd36ab4b15e5d35",
+    "url": "/static/js/main.ec230508.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
